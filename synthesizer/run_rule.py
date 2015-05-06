773a765d2bb111183d04bc31412f5822fa535163
Michael Xu <michaelx@nextcode.mit.edu>
2015-05-06 02:10:20 -0400

    running tests

--- synthesizer/run_rule.py
+++ synthesizer/run_rule.py
@@ -1,4 +1,4 @@
-import random, sys
+import random, sys, shutil, os
 
 def genFileName():
     uniq =  ''.join(random.choice('0123456789ABCDEF') for i in range(10))
@@ -8,29 +8,49 @@ def genFileName():
 code_name = sys.argv[1]
 
 lines = []
-f = open('sketch_output/%s.cpp')
+f = open('sketch_output/%s.cpp' % code_name)
 for line in f:
-    line.append(line)
+    lines.append(line)
 f.close()
 
 
 tests = []
 num_tests = input()
 
-for i in range(num_cases):
+for i in range(num_tests):
     line = raw_input()
     split = line.split(' ')
     tests.append((split[0].strip().lower(), split[1].strip().lower()))
 
 def testToAssert(test, index):
     ints = map(lambda x: str(ord(x) - ord('a')), list(test[0]))
-    arr_string = 'int test%d[%d] = '
-    arr_string = '{' + ints[0]
+    arr_string = '\n\n  // WORD: %s' % test[0]
+    arr_string += '\n  bool bool%d = true;\n' % index
+    arr_string += '  int test%d[%d] = ' % (index, len(test[0]))
+    arr_string += '  {' + ints[0]
     for i in ints[1:]:
         arr_string += ", " + i
     arr_string += '};\n'
-    if test[1].lower() == 'yes':
-        return "assert(rule(%s, %d));" % (arr_string, len(test[0]))
-    return "assert(!rule(%s, %d));" % (arr_string, len(test[0]))
+    arr_string += '  ANONYMOUS::rule(test%d, %d, bool%d);\n' % (index, len(test[0]), index)
+    desired = 'true' if test[1].lower() == 'yes' else 'false'
+    arr_string += '  result = result && (bool%d == %s);' % (index, desired)
+    return arr_string
 
-compile_command = 'g++ -o sketch_output/%s -I $SKETCH_HOME/include sketch_output/%s.cpp' % (code_name, code_name)
+lines.append('\nint main() {\n  bool result = true;\n')
+
+for i, test in enumerate(tests):
+    lines.append(testToAssert(test, i))
+
+lines.append('\n\n  if (result) {\n    printf("TESTS PASSED\\n");\n  } else {\n    printf("TESTS FAILED\\n");\n  }\n}')
+
+f = open('rule_programs/%s.cpp' % code_name, 'w')
+for line in lines:
+    f.write(line)
+f.close()
+
+shutil.copyfile('sketch_output/%s.h' % code_name, 'rule_programs/%s.h' % code_name)
+
+compile_command = 'g++ -o rule_programs/%s -I $SKETCH_HOME/include rule_programs/%s.cpp' % (code_name, code_name)
+
+os.system(compile_command)
+os.system('./rule_programs/%s' % code_name)
